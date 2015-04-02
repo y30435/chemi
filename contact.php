de1a62daeb3c90a3410ec1344ef7e0fd28cdfbde
박재훈 <jhr@wemakeprice.com>
2015-04-02 11:11:04 +0900

    네이버 지도로 변경

--- contact.php
+++ contact.php
@@ -8,30 +8,33 @@
                 <div class="col-lg-12">
                     <hr>
                     <h2 class="intro-text text-center">Contact
-                        <strong>business casual</strong>
+                        <strong>CHEMI HOUSE</strong>
                     </h2>
                     <hr>
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
-                    <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+                    <table cellpadding="0" cellspacing="0" width="462"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/index.nhn?menu=location&mapMode=0&lat=37.4326477&lng=127.1589798&dlevel=12&searchCoord=126.9641203%3B37.5323246&query=7LyA66%2B47ZWY7Jqw7Iqk&mpx=09170625%3A37.5323246%2C126.9641203%3AZ14%3A0.0028197%2C0.0014672&tab=1&enc=b64" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1427940346653_97817013" width="460" height="340" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td>  <table cellpadding="0" cellspacing="0" width="100%">  <tr>  <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;">   <span style="font-family: tahoma; font-size: 11px; color:#666;">2015.4.2</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/index.nhn?menu=location&mapMode=0&lat=37.4326477&lng=127.1589798&dlevel=12&searchCoord=126.9641203%3B37.5323246&query=7LyA66%2B47ZWY7Jqw7Iqk&mpx=09170625%3A37.5323246%2C126.9641203%3AZ14%3A0.0028197%2C0.0014672&tab=1&enc=b64" target="_blank">지도 크게 보기</a>  </td>  <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;">   <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span>  </td>  </tr>  </table> </td> </tr>  </table>
+<!--                     <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://map.naver.com/index.nhn?menu=location&mapMode=0&lat=37.4326477&lng=127.1589798&dlevel=12&searchCoord=126.9641203%3B37.5323246&query=7LyA66%2B47ZWY7Jqw7Iqk&mpx=09170625%3A37.5323246%2C126.9641203%3AZ14%3A0.0028197%2C0.0014672&tab=1&enc=b64"></iframe> -->
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <p>Phone:
-                        <strong>123.456.7890</strong>
+                        <strong>031-745-8831</strong>
                     </p>
                     <p>Email:
-                        <strong><a href="mailto:name@example.com">name@example.com</a></strong>
+                        <strong><a href="mailto:magicshow83@naver.com">magicshow83@naver.com</a></strong>
                     </p>
                     <p>Address:
-                        <strong>3481 Melrose Place
-                            <br>Beverly Hills, CA 90210</strong>
+                        <strong>B110 KRANZ TECHNO
+                        	<br>SANGDAEWON 1-DONG, JUNGWON-GU,
+                        	<br>SEONGNAM-SI 462-729</strong>
                     </p>
                 </div>
                 <div class="clearfix"></div>
             </div>
         </div>
 
+        <!-- 
         <div class="row">
             <div class="box">
                 <div class="col-lg-12">
@@ -69,6 +72,7 @@
                 </div>
             </div>
         </div>
+        -->
 
     </div>
     <!-- /.container -->
